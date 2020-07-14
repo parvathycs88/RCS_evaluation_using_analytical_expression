a58dcef971d8523c6163d5e210636e8e1497dedf
Parvathy <parvathycs88@gmail.com>
2020-07-14 14:34:16 +0100

    Tries to include function of theta and phi

--- Creating_database_metasurface_rcs_function_of_frequency_function_for_unit_cell_field.py
+++ Creating_database_metasurface_rcs_function_of_frequency_function_for_unit_cell_field.py
@@ -13,8 +13,8 @@ pi=np.pi
 df_10_6 = pd.read_excel('Efield_10degree_6mm.xlsx')
 df_85_10 = pd.read_excel('Efield_85degree_10mm.xlsx')
 
-theta_= np.arange(0,pi,0.087266).round(4)  #theta_= np.linspace(0,pi/2,90)
-phi_= np.arange(0,6.1959,0.087266).round(4)   #np.linspace(0,2*pi,360)
+theta_=  np.arange(0,pi,0.087266).round(4) #np.linspace(0,pi,10).round(4) #theta_= np.linspace(0,pi/2,90)
+phi_=  np.arange(0,6.1959,0.087266).round(4) #np.linspace(0,6.1959,10).round(4)  #phi_= np.linspace(0,2*pi,360)
 
 theta_value = df_10_6["Theta_radians"]
 phi_value = df_10_6["Phi_radians"]
@@ -35,7 +35,7 @@ df_v2["reflectionphase_unwrapped"] = np.unwrap((np.deg2rad(df_v2["reflectionphas
 
 #omsriramajayam
 
-def fun(x,i,angle1,angle2):
+def fun(x,i, angle1,angle2):
     L_v = x[:N**2]
     theta_v = x[N**2:]
     phase_pred = []
@@ -44,45 +44,39 @@ def fun(x,i,angle1,angle2):
             phase_pred.append(df_v1["reflectionphase_unwrapped"][i])
         if((t == 85) & (l == 10)):
             phase_pred.append(df_v2["reflectionphase_unwrapped"][i])   
-    print(phase_pred)
+    #print(phase_pred)
         #omsriramajayam
     reflection_phase = np.reshape(phase_pred, (N,N))
     #phase_pred = np.repeat(df["reflectionphase_in_radians"][i], N**2)
     #reflection_phase = np.reshape(phase_pred, (N,N))
     result = []
-    S = 0
-    S1 = []
-    S2 = []
-    S3 = np.zeros((len(phi_),len(theta_)))
+    S = 0 #np.zeros((len(theta_),len(phi_)))
+    #H = np.zeros((len(theta_),len(phi_)))
+    #directivity = np.zeros((len(theta_),len(phi_)))
+    #rcs = np.zeros((len(theta_),len(phi_)))
     #for angle1 in theta_value: 
         #for angle2 in phi_value: 
+    #for angle1 in range(len(theta_)):
+        #for angle2 in range(len(phi_)):    
     for m in range(N):
-        for n in range(N):    
-            S =  S + np.exp(-1j * (reflection_phase[m,n] + k[i]*D[i]*np.sin(theta)*((m-1/2)*np.cos(phi)+((n-1/2)*np.sin(phi)))))
-            #print(len(S))
-            #S =  S + np.exp(-1j * (reflection_phase[m,n] + k[i]*D[i]*np.sin(angle1)*((m-1/2)*np.cos(angle2)+((n-1/2)*np.sin(angle2)))))
-                    #S1 = df_10_6.loc[((df_10_6['Theta_radians'] == angle1) & (df_10_6['Phi_radians'] == angle2)), 'Abs(E)'] * S
-                    #print(S1)
-                    #print(len(S1))
-    #for angle1 in theta_: 
-        #for angle2 in phi_:
-            #S1 = df_10_6.loc[((df_10_6['Theta_radians'] == angle1) & (df_10_6['Phi_radians'] == angle2)), 'Abs(E)'] * S
-            #S2 = S2.extend(S1)
-            #print(S2)
-    #S = S1 #np.cos(theta) * S
-    #S = np.cos(theta) * (df_10_6['Abs(E)'].values.reshape(72,37)) * S
-    S = df_10_6.loc[((df_10_6['Phi_radians'] == phi_[angle1]) & (df_10_6['Theta_radians'] == theta_[angle2])), 'Abs(E)'] * S[angle1,angle2]
-    H = np.trapz(np.trapz(np.abs(S)**2*np.sin(theta),theta_),phi_) # integration using trapezoid function
+        for n in range(N):
+            S =  S  + np.exp(-1j * (reflection_phase[m,n] + k[i]*D[i]*np.sin(theta_[angle1])*((m-1/2)*np.cos(phi_[angle2])+((n-1/2)*np.sin(phi_[angle2])))))
+                    
+            S = np.multiply(S,(df_10_6.loc[((df_10_6['Phi_radians'] == phi_[angle2]) & (df_10_6['Theta_radians'] == theta_[angle1])), 'Abs(E)']))   
+            #print(S[int(theta_[angle1]),int(phi_[angle2])].shape)
+            #omsriramajayam 
+    H = np.abs(S)**2*np.sin(theta_[angle1]) #np.trapz(np.trapz(np.abs(S)**2*np.sin(theta_[angle1]),theta_),phi_) # integration using trapezoid function
     directivity = 4 * pi * np.abs(S)**2 / H
     rcs = (1/(4*pi*N**2)) * np.max(directivity)  
     result.append(10 * np.log10(rcs)) 
-    print(result)
-    return result,S
+    print(len(result))
+    return result
     
     
 rcs_over_frequency = {} 
 list_of_rcs_over_frequency = []
 list_for_many_combinations = pd.DataFrame([])
+ 
 
 dataframe = pd.read_excel('Length_Openingangle_V_Elements_Pattern_Design.xlsx', header = None)
 #x = np.zeros((99,200))  #Initialise numpy array x
@@ -96,18 +90,17 @@ Combination_number = 3
 
 for times in range(Combination_number):#dataframe.shape[0]):  
     for i in range(number_of_frequency_points):#len(df_v1)): 
-        for angle1 in range(len(phi_)):
-            for angle2 in range(len(theta_)):
+        for angle1 in range(len(theta_)):
+            for angle2 in range(len(phi_)):     
                 x[times][:N**2] = dataframe.iloc[times,:N**2].to_numpy()#np.array((t,l)).ravel() 
                 x[times][N**2:] = dataframe.iloc[times,N**2:].to_numpy()   
-        #print(type(fun(x[times],i)))
-                rcs_over_frequency['Combination_number_%d' %times][0:72] = fun(x[times],i,angle1,angle2)[0]
-                
-                list_of_rcs_over_frequency.extend(rcs_over_frequency['Combination_number_%d' %times])
-                S4 = fun(x[times],i,angle1,angle2)[1]
+        #print(type(fun(x[times],i)))                
+                rcs_over_frequency['Combination_number_%d_%d_%d' %(times,angle1,angle2)] = fun(x[times],i,int(theta_[angle1]),int(phi_[angle2]))
+                list_of_rcs_over_frequency.extend(rcs_over_frequency['Combination_number_%d_%d_%d' %(times,angle1,angle2)])
+        #S4 = fun(x[times],i,angle1,angle2)[1]
                 
 for j in range(Combination_number):
-    list_for_many_combinations['Combination_number_%d' %j] = list_of_rcs_over_frequency[number_of_frequency_points*j:number_of_frequency_points*j+number_of_frequency_points]#[len(df_v1)*j:len(df_v1)*j+len(df_v1)]
+    list_for_many_combinations['Combination_number_%d' %j] = list_of_rcs_over_frequency[(number_of_frequency_points*len(theta_)*len(phi_))*j:(number_of_frequency_points*len(theta_)*len(phi_))*j+(number_of_frequency_points*len(theta_)*len(phi_))]#[len(df_v1)*j:len(df_v1)*j+len(df_v1)]
 
 #list_for_many_combinations.to_excel("RCS_over_all_frequencies_for_all_combinations.xlsx") 
 
@@ -116,7 +109,7 @@ for k in range(list_for_many_combinations.shape[1]):
     plt.xlabel("Frequency GHz")
     plt.ylabel("RCS in dB")
     plt.title("RCS for %d combination of V1 and V2 from 6GHz to 14GHz \n" %k, loc = 'right')
-    plt.plot(df_v1["frequency"][0:number_of_frequency_points],list_for_many_combinations['Combination_number_%d' %k])
+    plt.plot(np.repeat([df_v1["frequency"][0:number_of_frequency_points]],len(theta_)*len(phi_)*number_of_frequency_points,0) ,list_for_many_combinations['Combination_number_%d' %k])
     #plt.savefig("RCS over frequency considering unit field function for Combination_number_%d_%%d.png" %k %number_of_frequency_points)
 plt.show()
 plt.ion() # helps to come to next line in command window without cosing figures
